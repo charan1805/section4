210fe104a773466a66d630888df4d05f3b8f6672
charan1805 <sreecharanmudhiraj@gmail.com>
2023-09-04 20:51:49 +0530

    first class

---
+++ SecurityApplications/SecurityApplications/SecurityApplications/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityApplications.Startup))]
+namespace SecurityApplications
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
